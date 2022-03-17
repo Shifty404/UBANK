f4e19a5531079ec22aa3d8786c40e5551d3828c3
Shifty404 <sishmam211040@bscse.uiu.ac.bd>
2022-03-17 08:28:55 +0600

    shifty
    Open Scene

---
+++ Project/src/OpenSceneController.java
@@ -0,0 +1,37 @@
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author shifty
+ */
+public class OpenSceneController implements Initializable {
+
+    @FXML
+    private Button SignUpButton;
+    @FXML
+    private Button LogInButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void SignUpButtonPush(ActionEvent event) {
+    }
+
+    @FXML
+    private void LogInButtonPush(ActionEvent event) {
+    }
+    
+}
