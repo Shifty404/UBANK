de28fb529f0d6bb74ae00b607e8ec9dff3aa4ad5
alifayeasmin <alifabristy07@gmail.com>
2022-03-22 10:46:52 +0600

    class update

--- Project/src/CashDepositSceneController.java
+++ Project/src/CashDepositSceneController.java
@@ -3,9 +3,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class CashDepositSceneController implements Initializable {
 
@@ -20,7 +24,12 @@ public class CashDepositSceneController implements Initializable {
     }    
 
     @FXML
-    private void DepositConfirmButtonPushed(ActionEvent event) {
+    private void DepositConfirmButtonPushed(ActionEvent event) throws Exception {
+        Parent groot = FXMLLoader.load(getClass().getResource("AppScene.fxml"));
+        Stage stage = (Stage)DepositConfirmButton.getScene().getWindow();
+        Scene scene = new Scene(groot);
+        stage.setScene(scene);
+        stage.show();
     }
     
 }
