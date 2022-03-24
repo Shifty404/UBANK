d4cdc4547b9ad0353aad673eb4f4ca1d1e227df4
Shifty404 <sishmam211040@bscse.uiu.ac.bd>
2022-03-24 20:25:01 +0600

    daliy prac

--- Project/src/PayBillSceneController.java
+++ Project/src/PayBillSceneController.java
@@ -1,40 +1,12 @@
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 
 public class PayBillSceneController implements Initializable {
-
-    @FXML
-    private Button eletricBillButton;
-    @FXML
-    private Button internetBillButton;
-    @FXML
-    private Button waterBillButton;
-    @FXML
-    private Button gasBillButton;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
     }    
-
-    @FXML
-    private void eletricBillButtonPushed(ActionEvent event) {
-    }
-
-    @FXML
-    private void internetBillButtonPushed(ActionEvent event) {
-    }
-
-    @FXML
-    private void waterBillButtonPushed(ActionEvent event) {
-    }
-
-    @FXML
-    private void gasBillButtonPushed(ActionEvent event) {
-    }
     
 }
