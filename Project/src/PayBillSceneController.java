db31e8f64aac45be89319f12c263f1f16b8e2140
AtikaJasia <101453372+AtikaJasia@users.noreply.github.com>
2022-04-12 23:18:14 +0600

    Jasia

--- Project/src/PayBillSceneController.java
+++ Project/src/PayBillSceneController.java
@@ -3,12 +3,30 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+<<<<<<< Updated upstream
 import javafx.scene.control.Label;
+=======
+import javafx.scene.control.Button;
+>>>>>>> Stashed changes
 
 public class PayBillSceneController implements Initializable {
 
     @FXML
+<<<<<<< Updated upstream
     private Label electricityBillLabel;
+=======
+    private Button electricityPay;
+    @FXML
+    private Button internetPay;
+    @FXML
+    private Button waterPay;
+    @FXML
+    private Button gasPay;
+    @FXML
+    private Button governmentPay;
+    @FXML
+    private Button creditCardPay;
+>>>>>>> Stashed changes
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
