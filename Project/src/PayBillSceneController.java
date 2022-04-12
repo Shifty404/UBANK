6abb44a8b5f269e2c3596cdac3bf92a4c5c15ed2
AtikaJasia <101453372+AtikaJasia@users.noreply.github.com>
2022-04-13 02:05:21 +0600

    pay bill multy tab

--- Project/src/PayBillSceneController.java
+++ Project/src/PayBillSceneController.java
@@ -9,6 +9,16 @@ public class PayBillSceneController implements Initializable {
 
     @FXML
     private Label electricityBillLabel;
+    @FXML
+    private Label electricityBillLabel1;
+    @FXML
+    private Label electricityBillLabel2;
+    @FXML
+    private Label electricityBillLabel3;
+    @FXML
+    private Label electricityBillLabel4;
+    @FXML
+    private Label electricityBillLabel5;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
