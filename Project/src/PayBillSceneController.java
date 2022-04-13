4a89e86cd0fc27713ce103fa68dd58bd6e31fa7a
AtikaJasia <101453372+AtikaJasia@users.noreply.github.com>
2022-04-13 22:20:09 +0600

    pay bill update

--- Project/src/PayBillSceneController.java
+++ Project/src/PayBillSceneController.java
@@ -1,9 +1,12 @@
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class PayBillSceneController implements Initializable {
 
@@ -19,9 +22,57 @@ public class PayBillSceneController implements Initializable {
     private Label electricityBillLabel4;
     @FXML
     private Label electricityBillLabel5;
+    @FXML
+    private TextField electricBillTextField;
+    @FXML
+    private Button electricBillPayButton;
+    @FXML
+    private TextField internetBillTextField;
+    @FXML
+    private Button internetBillPayButton;
+    @FXML
+    private TextField gasBillTextField;
+    @FXML
+    private Button gasBillPayButton;
+    @FXML
+    private TextField waterBillTextField;
+    @FXML
+    private Button waterBillPayButton;
+    @FXML
+    private TextField governmentFeesTextField;
+    @FXML
+    private Button governmentFeesPayButton;
+    @FXML
+    private TextField creditCardTextField;
+    @FXML
+    private Button creditCardBillPayButton;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
     }    
+
+    @FXML
+    private void electricBillPayButtonPushed(ActionEvent event) {
+    }
+
+    @FXML
+    private void internetBillPayButtonPushed(ActionEvent event) {
+    }
+
+    @FXML
+    private void gasBillPayButtonPushed(ActionEvent event) {
+    }
+
+    @FXML
+    private void waterBillPayButtonPushed(ActionEvent event) {
+    }
+
+    @FXML
+    private void governmentFeesPayButtonPushed(ActionEvent event) {
+    }
+
+    @FXML
+    private void creditCardBillPayButtonPushed(ActionEvent event) {
+    }
     
 }
