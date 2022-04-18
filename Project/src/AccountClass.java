63a30efea1b5525cb7fd5f198cb2c0cd68fbf592
Shifty404 <sishmam211040@bscse.uiu.ac.bd>
2022-04-18 18:13:45 +0600

    update by shifty

--- Project/src/AccountClass.java
+++ Project/src/AccountClass.java
@@ -1,7 +1,24 @@
 
 public class AccountClass {
     
-    
-    
+    public String name; 
+    public int accountNumber;
+    public String mail;
+    public int birthday;
+    public String gender;
+    public int phoneNumber;
+    public int yearlyIncome;
+    public String password;
+
+    public AccountClass(String name, int accountNumber, String mail, int birthday, String gender, int phoneNumber, int yearlyIncome, String password) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.mail = mail;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.yearlyIncome = yearlyIncome;
+        this.password = password;
+    }
     
 }
